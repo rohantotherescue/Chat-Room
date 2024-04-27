3a7cbc61cb1106b7324d786f8bcabecce3651e3d
rohantotherescue <rohantotherescue@gmail.com>
2024-04-27 15:51:11 +0530

    created server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const {createServer} = require("http")
+const { Server }= require("socket.io")
+
+const httpServer= createServer()
+const socket= new Server(httpServer, {})
+
+socket.on("connection", (socket)=> {
+    console.log(socket)
+})
+
+httpServer.listen(3000, ()=>{
+    console.log("server is connected")
+})
